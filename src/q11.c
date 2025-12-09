2eb8cbb54db8a63ee7fde3f3d646afaca92ec9a9
Aadarsha Shrestha <shresthaaadarsha4321@gmail.com>
2025-12-10 01:56:40 +0545

    Implement single-line expression for calculations

--- src/q11.c
+++ src/q11.c
@@ -1,1 +1,12 @@
-// Write a C expression that performs the following operations in a single line: increment a variable by 1, multiply it by 3, and subtract 10.
+#include <stdio.h>
+
+int main() {
+    int num;
+    scanf("%d", &num);
+
+    int result = ((++num) * 3) - 10;
+
+    printf("Result = %d\n", result);
+
+    return 0;
+}
