aec2ace4b49f811c24c86afa896348eb8d5de27d
Aadarsha Shrestha <shresthaaadarsha4321@gmail.com>
2025-12-10 01:55:18 +0545

    Implement check for positive and even number

--- src/q15.c
+++ src/q15.c
@@ -1,1 +1,14 @@
-// Write an expression that checks if a number is both positive and even.
+#include <stdio.h>
+
+int main() {
+    int num;
+    scanf("%d", &num);
+
+    if ((num > 0) && (num % 2 == 0)) {
+        printf("%d is positive and even\n", num);
+    } else {
+        printf("%d is not positive and even\n", num);
+    }
+
+    return 0;
+}
